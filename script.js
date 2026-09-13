c87b0d1f6cc8c2c12f63e55bbc4bd7df064a4f31
GhotaiPayenda <ghotaipayenda43@gmail.com>
2026-09-13 14:01:34 +0430

    adds the footer of hero section

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const navbarToggle = document.querySelector(".navbar-toggle");
 const header = document.querySelector("header");
 
+const star = document.querySelector(".star");
+
+
 navbarToggle.addEventListener('click', () => {
     header.classList.toggle('active');
 });
+
+
