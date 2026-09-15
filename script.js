7108031d6be2b21e2dc25d68b7ff1e01875990f1
GhotaiPayenda <ghotaipayenda43@gmail.com>
2026-09-15 16:35:12 +0430

    add hover afect to the cards

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 const navbarToggle = document.querySelector(".navbar-toggle");
 const header = document.querySelector("header");
 
-const star = document.querySelector(".star");
-
-
 navbarToggle.addEventListener('click', () => {
     header.classList.toggle('active');
 });
@@ -13,8 +10,7 @@ const elements = document.querySelectorAll(
     ".scroll-reveal, .scroll-left, .scroll-right, .scroll-scale, .scroll-stagger"
 );
 
-const observer = new IntersectionObserver(
-    (entries) => {
+const observer = new IntersectionObserver((entries) => {
         entries.forEach((entry) => {
 
             if (entry.isIntersecting) {
@@ -23,9 +19,6 @@ const observer = new IntersectionObserver(
 
         });
     },
-    {
-        threshold: 0.15
-    }
 );
 
 elements.forEach((element) => {
